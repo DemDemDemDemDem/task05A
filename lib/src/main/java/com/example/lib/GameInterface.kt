5bf145649f3966f9e36e1898b27e1f9b966aba95
Dem <danpajevic@gmail.com>
2020-04-30 13:51:33 +0100

    created the interface for the ui and the logic

---
+++ lib/src/main/java/com/example/lib/GameInterface.kt
@@ -0,0 +1,18 @@
+package com.example.lib
+
+interface GameInterface {
+    val mColums: Int
+    val mRows: Int
+
+    //Internal representation of the state of the game - the grid of coloured tokens
+    var mData: Array<IntArray>
+
+    //Returns the state of the game grid at a specied column and
+    // row number
+    fun getToken(column: Int, row: Int): Boolean
+
+    //Changes the contents of the game grid at a specified row and column
+    fun playToken(column: Int, player: Int): Boolean
+
+
+}
