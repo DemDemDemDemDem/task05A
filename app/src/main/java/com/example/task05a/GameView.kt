d6e518b43d94a465f06d83f6e29675101a3e9d61
Dem <danpajevic@gmail.com>
2020-04-30 13:21:39 +0100

    GameView Class Started

---
+++ app/src/main/java/com/example/task05a/GameView.kt
@@ -0,0 +1,59 @@
+package com.example.task05a
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class GameView: View {
+    constructor(context: Context?)  : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr)
+
+    private val colCount = 7
+    private val rowCount = 10
+
+    private var mGridPaint: Paint
+    private var mNoPlayerPaint: Paint
+
+    init {
+        mGridPaint = Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.BLUE
+        }
+        mNoPlayerPaint = Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.WHITE
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        val chosenDiameter: Float
+        var tokenAtPos: Int
+        var paint: Paint
+
+        val viewWidth: Float = width.toFloat()
+        val viewHeight: Float = height.toFloat()
+
+
+        val diameterX: Float = viewWidth / colCount.toFloat()
+        val diameterY: Float = viewHeight / rowCount.toFloat()
+
+        //Choose the smallest of the two
+        if (diameterX < diameterY)
+            chosenDiameter = diameterX
+        else
+            chosenDiameter = diameterY
+
+
+
+
+    }
+
+
+}
